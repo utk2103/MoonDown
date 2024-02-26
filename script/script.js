3b44023bb41e298bb045f1b574e298a3669b8b1b
Utkarsh <btoshine774@gmail.com>
2024-02-27 02:45:26 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -3,3 +3,23 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+// var a = document.querySelector("#elem1")
+// var image = a.getAttribute("data-image")
+// console.log(image)
+
+
+var fixed = document.querySelector("#fixed-image")
+var elemC = document.querySelector("#elem-container")
+elemC.addEventListener("mouseenter", function(){
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function(){
+    fixed.style.display = "none"
+})
+
+
+var elem1 = document.querySelector("#elem1")
+elem1.addEventListener("mouseenter",function(){
+    var image = elem1.getAttribute("data-image")
+    fixed.style.backgroundImage = `url($(image))`
+})
