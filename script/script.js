7d2094c2e00b532c86ad82ab64d2965ef933bbb2
Utkarsh <btoshine774@gmail.com>
2024-05-07 00:03:39 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -8,7 +8,8 @@ const scroll = new LocomotiveScroll({
 // console.log(image)
 
 
-var fixed = document.querySelector("#fixed-image")
+function page4Animation(){
+    var fixed = document.querySelector("#fixed-image")
 var elemC = document.querySelector("#elem-container")
 elemC.addEventListener("mouseenter", function(){
     fixed.style.display = "block"
@@ -17,9 +18,45 @@ elemC.addEventListener("mouseleave", function(){
     fixed.style.display = "none"
 })
 
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
 
-var elem1 = document.querySelector("#elem1")
-elem1.addEventListener("mouseenter",function(){
-    var image = elem1.getAttribute("data-image")
-    fixed.style.backgroundImage = `url($(image))`
+}
+// var elem1 = document.querySelector("#elem1")
+// elem1.addEventListener("mouseenter",function(){
+//     var image = elem1.getAttribute("data-image")
+//     fixed.style.backgroundImage = `url($(image))`
+// })
+
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+}
+swiperAnimation()
+page4Animation()
+
+var menu = document.querySelector("nav h3")
+var full = document.querySelector("#full-scr")
+var navimg = document.querySelector("#nav img")
+var flag = 0
+menubar.addEventListener("click",function(){
+        if(flag ==0){
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        }else{
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    
 })
